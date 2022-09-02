ea20d969179133aa846d427d8c9594333629f4d3
Frederic Lavigne <frederic.lavigne@ibm.com>
2022-09-02 11:10:31 -0500

    with ibm provider

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 from constructs import Construct
-from cdktf import App, TerraformStack
+from cdktf import App, TerraformStack, TerraformOutput, TerraformVariable
 
+from imports.ibm import ResourceGroup, IbmProvider
 
 class MyStack(TerraformStack):
   def __init__(self, scope: Construct, ns: str):
     super().__init__(scope, ns)
 
-    # define resources here
+    IbmProvider(self, "ibm")
 
+    basename = TerraformVariable(self,
+      "basename",
+      type="string",
+      default="cdktf-gs",
+      description="Prefix to be used to name resources"
+    )
+
+    # define resources here
+    group = ResourceGroup(self,
+      'group',
+      name=f"{basename.string_value}-group"
+    )
+
+    TerraformOutput(self,
+      'group_id',
+      value=group.id
+    )
 
 app = App()
 MyStack(app, "ibmcloud-cdktf-getting-started")
